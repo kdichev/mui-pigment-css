566c336a59f2232665d2fb9b41954dea2143bd68
kdichev <konstantin.dichev@gmail.com>
2024-09-21 16:53:15 +0300

    add pigment to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
+import { withPigment } from "@pigment-css/nextjs-plugin";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPigment(nextConfig);
