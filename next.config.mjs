a360dcbb9049ed1892a9817a5c6f25e7d47a646a
kdichev <konstantin.dichev@gmail.com>
2024-09-21 16:53:52 +0300

    add example theme to next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,17 @@ import { withPigment } from "@pigment-css/nextjs-plugin";
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withPigment(nextConfig);
+export default withPigment(nextConfig, {
+  theme: {
+    colors: {
+      primary: "tomato",
+      secondary: "cyan",
+    },
+    spacing: {
+      unit: 8,
+    },
+    typography: {
+      fontFamily: "sans-serif",
+    },
+  },
+});
