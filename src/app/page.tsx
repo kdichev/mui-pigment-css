e66564b0882eb7b14b802c7a427ae230311bb8ce
kdichev <konstantin.dichev@gmail.com>
2024-09-21 16:54:13 +0300

    add components to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+import { Button } from "@mui/material";
+import { styled } from "@pigment-css/react";
+
+const Heading = styled("h1")(({ theme }) => ({
+  color: theme.colors.primary,
+  fontSize: theme.spacing.unit * 4,
+  fontFamily: theme.typography.fontFamily,
+}));
+
 const HomePage = async () => {
-  return <div>mui-pigment-css</div>;
+  return (
+    <div>
+      <Heading>Heading</Heading>
+      <Button variant="contained" color="primary">
+        mui-pigment-css
+      </Button>
+    </div>
+  );
 };
 
 export default HomePage;
